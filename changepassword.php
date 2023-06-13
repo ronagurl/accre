44cab371ee83d24aac4a7ff7791a3f2cf68e2de8
ronagurl <131933550+ronagurl@users.noreply.github.com>
2023-06-13 23:36:41 +0800

    Add files via upload

---
+++ changepassword.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="css/changepassword.css" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100;0,400;0,800;1,300&display=swap"
+    />
+  </head>
+  <body>
+    <div class="macbook-pro-14-4">
+      <div class="macbook-pro-14-4-child"></div>
+      <div class="macbook-pro-14-4-item" id="rectangle1"></div>
+      <div class="change-password">Change Password</div>
+      <div class="macbook-pro-14-4-inner"></div>
+      <div class="new-password">new password</div>
+      <img class="rectangle-icon" alt="" src="img/closedeyes.png" />
+
+      <div class="rectangle-div"></div>
+      <div class="confirm-password">confirm password</div>
+      <img
+        class="macbook-pro-14-4-child1"
+        alt=""
+        src="img/closedeyes.png"
+      />
+
+      <div class="macbook-pro-14-4-child2"></div>
+      <div class="admin123">admin123</div>
+      <div class="macbook-pro-14-4-child3"></div>
+      <div class="old-password">old password</div>
+      <img
+        class="macbook-pro-14-4-child4"
+        alt=""
+        src="img/closedeyes.png"
+      />
+
+      <div class="macbook-pro-14-4-child3"></div>
+      <div class="old-password">old password</div>
+      <img
+        class="macbook-pro-14-4-child4"
+        alt=""
+        src="img/closedeyes.png"
+      />
+
+      <div class="macbook-pro-14-4-child7"></div>
+      <div class="accreditation">ACCREDITATION</div>
+      <div class="r-e-p">R E P O S I T O R Y    S Y S T E M</div>
+      <div class="change-password1">Change Password</div>
+      <img
+        class="macbook-pro-14-4-child8"
+        alt=""
+        src="img/cvsulogo.png"
+      />
+    </div>
+
+    <script>
+      var rectangle1 = document.getElementById("rectangle1");
+      if (rectangle1) {
+        rectangle1.addEventListener("click", function (e) {
+          // Please sync "MacBook Pro 14" - 3" to the project
+        });
+      }
+      </script>
+  </body>
+</html>
